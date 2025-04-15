5e33a542d71244079b26049fdf50d3ab173e04e8
joshuanelsondev <joshuanelsondev@gmail.com>
2025-04-15 08:52:47 -0400

    complete tests

--- src/main/java/org/example/UserSet.java
+++ src/main/java/org/example/UserSet.java
@@ -1,14 +1,54 @@
 package org.example;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Set;
 
 public class UserSet {
+    HashSet<String> users = new HashSet<String>();
 
-
-    public void addUser(String user) {
-
+    public boolean addUser(String userName) {
+        if (userName == null || userName.trim().isEmpty()) throw new IllegalArgumentException();
+        return users.add(userName);
     }
+
+    public boolean removeUser(String userName) {
+        return users.remove(userName);
+    }
+
+    public boolean containsUser(String userName) {
+        return users.contains(userName);
+    }
+
+    public int getUserCount() {
+        return users.size();
+    }
+
+    public ArrayList<String> getUsersWithPrefix(String prefix) {
+        if (prefix == null) throw new IllegalArgumentException();
+        ArrayList<String> usersWithPrefix = new ArrayList<>();
+        for (String user : users) {
+           if (user.contains(prefix)) {
+               usersWithPrefix.add(user);
+           }
+        }
+
+        return usersWithPrefix;
+    }
+
+    public boolean areAllUsersUnique(String[] arr) {
+       if (arr == null)  throw new IllegalArgumentException();
+       UserSet currentUserSet = new UserSet();
+
+       for (String str : arr) {
+           if (currentUserSet.containsUser(str)) {
+               return false;
+           }
+
+           currentUserSet.addUser(str);
+       }
+
+       return true;
+    }
+
 }
 
